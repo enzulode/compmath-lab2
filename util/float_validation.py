a04b4dd7610287d2c0e850d40ba834b923dce8b7
Vladislav Andreev <andreevvlad443@gmail.com>
2024-04-09 03:58:21 +0300

    feat: float string validation

---
+++ util/float_validation.py
@@ -0,0 +1,9 @@
+def is_float(data: str):
+  try:
+    float(data)
+    return True
+  except ValueError:
+    return False
+
+def check_input_is_float(text, action):
+  return action != '1' or is_float(text)
