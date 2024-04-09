9d1851f2daf40776c0385e25a5d7fa5746604db6
Vladislav Andreev <andreevvlad443@gmail.com>
2024-04-09 23:00:01 +0300

    fix: dialog window size & label wraplength adjusted

--- ui/lab2/windows/dialog_window__window.py
+++ ui/lab2/windows/dialog_window__window.py
@@ -5,9 +5,9 @@ class DialogWindow(CTk):
   
   def __init__(self, window_title: str, error_text: str):
     super().__init__()
-    self.geometry('300x200')
+    self.geometry('600x200')
     self.title(window_title)
 
-    label: CTkLabel = CTkLabel(self, text=error_text, height=40, wraplength=400)
+    label: CTkLabel = CTkLabel(self, text=error_text, height=40, wraplength=500)
     label.pack(expand=True)
     
