3b9bd7823f2ba0ee1634dd8f4460f55dbbe37d6f
Vladislav Andreev <andreevvlad443@gmail.com>
2024-04-09 03:27:45 +0300

    feat: basic iterative method corner cases covered

--- math/equation_math.py
+++ math/equation_math.py
@@ -41,17 +41,24 @@ def newton_method(a: float, b: float, eq: EquationOne, eps: float = 0.01) -> flo
   return x, n
 
 def basic_iterative_method(a: float, b: float, eq: EquationOne, eps: float = 0.01) -> float:
-  derivative_a: float = eq.dfunc(a)
-  derivative_b: float = eq.dfunc(b)
-  lambd: float = -1 / max(derivative_a, derivative_b)
-  x: float = a if derivative_a > derivative_b else b
+  if (not validate__left_border_less_than_right(a, b)):
+    raise Exception('Invalid interval: left border should be less than right')
+
+  if (not validate__single_root(a, b, eq)):
+    raise Exception('Invalid interval: the requirement about singe root in the interval was not fullfilled')
   
-  # covergence check
+  lmbda = -1 / max(eq.dfunc(a), eq.dfunc(b))
+  n = 0
+  x = a if eq.dfunc(a) > eq.dfunc(b) else b
+  q = max(abs(1 + lmbda * eq.dfunc(a)), abs(1 + lmbda * eq.dfunc(b)))
+  if (q > 1):
+    raise Exception(f'There is no convergence on this interval, q = {q}')
 
   while(abs(eq.func(x)) > eps):
-    x += lambd * eq.func(x)
+    n += 1
+    x += lmbda * eq.func(x)
 
-  return x
+  return x, n
 
 def validate__left_border_less_than_right(a: float, b: float) -> bool:
   return a < b
